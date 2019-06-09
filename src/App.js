04fba063860ff85f0f81634d29fdc81f456dd868
Chase Southard <chase.southard@gmail.com>
2019-06-09 12:40:11 -0400

    add bootstrap / reactstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 import Squad from './components/Squad'
 
 
 function App() {
   return (
-    <Squad/>
+    <Container>
+      <Row>
+        <Col sm="12" md={{ size: 12 }}>
+          <Squad/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
